13cf98eb87cd0d7d978eaad500728d2bf2f88a84
Daniel C. Jones <djones3@fredhutch.org>
2021-10-07 18:41:17 -0700

    Add RotGroupConvTranspose layer.

--- src/GCNN.jl
+++ src/GCNN.jl
@@ -1,10 +1,14 @@
 module GCNN
 
-export RotGroupConv
+export RotGroupConv, RotGroupConvTranspose
 
 using Flux
-using Flux: conv, convfilter, glorot_uniform, @functor
+using Flux: conv, ∇conv_data, convfilter, glorot_uniform, @functor, DenseConvDims
 using SparseArrays
+
+
+# TODO:
+#   Add bias to layers. (Does this need to be rotated along with weights?)
 
 """
 Add an extra dimension to the end of an array.
@@ -104,7 +108,7 @@ Base.length(rg::RotGroup) = length(rg.Rs)
 
     
 """
-Group convolutional layer lifting Z2 to SE2N with N orientations.
+Group convolutional layer.
 """
 struct RotGroupConv{F, WT<:AbstractArray, RT<:AbstractSparseMatrix}
     σ::F
@@ -161,6 +165,97 @@ function (lyr::RotGroupConv)(x::AbstractArray{T,5}) where {T}
     #return cat([expand_dims(lyr.σ.(conv(Rx, Rw))) for (Rx, Rw) in zip(Rxs, Rws)]..., dims=5)
 
     return cat([expand_dims(lyr.σ.(conv(Rxs[k], Rws[k], stride=lyr.stride))) for k in 1:nrots]..., dims=5)
+end
+
+
+"""
+Group transposed convolutional layer.
+"""
+struct RotGroupConvTranspose{F, WT<:AbstractArray, RT<:AbstractSparseMatrix, M, N}
+    rg::RotGroup{RT}
+    weight::WT
+    σ::F
+    stride::Int
+    pad::NTuple{M,Int}
+    dilation::NTuple{N,Int}
+end
+
+@functor RotGroupConvTranspose
+
+
+"""
+    RotGroupConvTranspose(nrots, filter, in => out, σ = identity)
+
+Construct a Group CNN (G-CNN) transposed convolution layer with `nrots` equally
+spaced rotations, and a `(filter, filter)` convolution kernel, with `in` input
+channels and `out` output channels, with an optional elementwise nonlinearity
+`σ`.
+"""
+function RotGroupConvTranspose(
+        nrots::Integer, filter::Integer, ch::Pair{<:Integer,<:Integer}, σ=identity;
+        init=glorot_uniform, stride::Int=1, pad=0, dilation=1, use_bias::Bool=true)
+
+    weight = convfilter((filter, filter), reverse(ch); init)
+
+    # bias = create_bias(weght, use_bias, size(weight, N))
+    rg = RotGroup(filter, filter, nrots)
+
+    return RotGroupConvTranspose(rg, weight, σ, stride, pad, dilation)
+end
+
+
+function RotGroupConvTranspose(
+        rg::RotGroup, weight::AbstractArray{T, N}, σ, stride, pad, dilation) where {T, N}
+    dilation = Flux.expand(Val(N-2), dilation)
+    pad = Flux.calc_padding(ConvTranspose, pad, size(weight)[1:N-2], dilation, stride)
+    return RotGroupConvTranspose(rg, weight, σ, stride, pad, dilation)
+end
+
+
+function conv_transpose_dims(c::RotGroupConvTranspose, xsize::NTuple{N, Int}) where {N}
+    # Calculate size of "input", from ∇conv_data()'s perspective...
+    combined_pad = (c.pad[1:2:end] .+ c.pad[2:2:end])
+    I = (xsize[1:end-2] .- 1).*c.stride .+ 1 .+ (size(c.weight)[1:end-2] .- 1).*c.dilation .- combined_pad
+    C_in = size(c.weight)[end-1]
+    batch_size = xsize[end]
+    # Create DenseConvDims() that looks like the corresponding conv()
+    return DenseConvDims((I..., C_in, batch_size), size(c.weight);
+                        stride=c.stride,
+                        padding=c.pad,
+                        dilation=c.dilation,
+    )
+end
+
+
+"""
+Rotation group transposed convolution applied to an array with shape [width,
+height, in_channels, batches] and producing an array of shape [width, height,
+out_channels, batches, nrotations].
+"""
+function (lyr::RotGroupConvTranspose)(x::AbstractArray{T,4}) where {T}
+    # Rotate the weights according to each rotation matrix, apply each matrix, then concatenate.
+    cdims = conv_transpose_dims(lyr, size(x))
+    return cat([expand_dims(lyr.σ.(∇conv_data(x, Rw, cdims))) for Rw in lyr.rg(lyr.weight)]..., dims=5)
+end
+
+
+"""
+Rotation group transposed convolution applied to an array with shape `[width,
+height, in_channels, batches, nrotations]` and producing an array of shape
+`[width, height, out_channels, batches, nrotations]`.
+"""
+function (lyr::RotGroupConvTranspose)(x::AbstractArray{T,5}) where {T}
+    nrots = length(lyr.rg)
+    @assert size(x, 5) == nrots
+
+    Rws = lyr.rg(lyr.weight)
+    Rxs = [view(x, :, :, :, :, i) for i in 1:nrots]
+    cdims = conv_transpose_dims(lyr, size(x)[1:end-1])
+
+    # gradients don't work with zip
+    #return cat([expand_dims(lyr.σ.(conv(Rx, Rw))) for (Rx, Rw) in zip(Rxs, Rws)]..., dims=5)
+
+    return cat([expand_dims(lyr.σ.(∇conv_data(Rxs[k], Rws[k], cdims))) for k in 1:nrots]..., dims=5)
 end
 
 
